d86f7aa5b76fa15bbd4e67349b09ca1f9f7ed64b
THEkingmay <beerranee@gmail.com>
2025-02-14 23:02:35 +0700

    update see detail by type for mounth and year

--- src/components/Monthlist.jsx
+++ src/components/Monthlist.jsx
@@ -1,8 +1,9 @@
 import { useParams } from "react-router"
 import { Link } from "react-router"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 function MonthList(){
     console.log('Month list rerender')
+
     const { month , year } = useParams() 
     useEffect(() => {
             window.scrollTo(0, 0);
@@ -58,12 +59,61 @@ function MonthList(){
             </Link>
         ));
     } 
-    
+    const showTotalByType = () =>{
+        let food_total_ex= 0 
+        let stuff_total_ex = 0
+        let travel_total_ex = 0
+        let other_total_ex = 0
+
+        const DATA = getData()
+        DATA.forEach(data=>{
+            console.log(data)
+            if(data.isExpense){
+                switch(data.selectType){
+                    case 'food' :
+                    food_total_ex+=Number(data.amount)
+                    break;
+                    case 'stuff' :
+                    stuff_total_ex+=Number(data.amount)
+                    break;
+                    case 'travel' :
+                    travel_total_ex+=Number(data.amount)
+                    break;
+                    case 'other' :
+                    other_total_ex+=Number(data.amount)
+                    break;
+                }
+            }
+        })
+        return(
+            <div className="mt-2 flex justify-between">
+                <div className="shadow bg-green-100 rounded  w-full p-3 flex justify-center flex-col items-center">
+                    <div className="border-b mb-2 border-green-500">Food expense</div>
+                    <div>{food_total_ex}</div>
+                </div>                
+                <div className="ml-2 shadow  bg-blue-100 rounded  w-full p-3 flex justify-center flex-col items-center">
+                    <div className="border-b mb-2 border-blue-500">Stuff expense</div>
+                    <div>{stuff_total_ex}</div>
+                </div>
+                <div className="ml-2 shadow w-full p-3 flex justify-center flex-col items-center bg-orange-100 rounded ">
+                    <div className="border-b mb-2 border-orange-500">Travel expense</div>
+                    <div>{travel_total_ex}</div>
+                </div>
+                <div className="ml-2 shadow w-full p-3 flex justify-center flex-col items-center  bg-gray-100 rounded">
+                    <div className="border-b mb-2 border-gray-500">Other expense</div>
+                    <div>{other_total_ex}</div>
+                </div>
+            </div>
+        )
+    }
     return(
         <div className="p-1">
             <div className="flex justify-between">
                 <span className='underline'>{ month}-{year}</span>
                 <span>{calTotalInMonth()}</span>
+            </div>
+            <div>
+                {showTotalByType()}
             </div>
             <div>
                 {getAllDay()}
