456e133dd798c3771ab9a8acc66bed1e67ca7beb
THEkingmay <beerranee@gmail.com>
2025-06-06 12:21:34 +0700

    long time no see

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Pages from "./pagesRoute/PageRoute";
 import { DataProvider } from "./datacontext/DataConPro";
 
 export default function App(){
+  useEffect(()=>{
+    console.log("TEST PUSH FROM CLONED PROJECT")
+  },[])
   return(
     <DataProvider>
           <Pages/>
